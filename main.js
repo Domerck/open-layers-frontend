8507719fad197e6ae8f25a264b2c655084fe0dad
Domerck <domerckbrigson.quiton@msugensan.edu.ph>
2022-04-21 15:04:15 +0800

    add layer

--- main.js
+++ main.js
@@ -108,10 +108,62 @@ var mousePosition = new ol.control.MousePosition({
 
 map.addControl(mousePosition);
 
-//Pop up
 
 
+// Get the modal
+var modal = document.getElementById("myModal");
+// Get the button that opens the modal
+var btn = document.getElementById("openModal");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
+// When the user clicks the button, open the modal 
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+//add to map
+
+var srvControl = new ol.control.Control({
+    element: btn
+})
+
+map.addControl(srvControl);
+
+//start: add service to code (not finished yet)
+//Add Layer To List of Layers 
+
+function addLi() {
+
+    var title = document.getElementById('layerTitle').value,
+        url = document.getElementById('layerUrl').value;
+
+    var listNode = document.getElementById('layerUL');
+    var liNode = document.createElement("LI");
+
+    var titleNode = document.createTextNode(title),
+        urlNode = document.createTextNode(url);
+
+    liNode.appendChild(titleNode);
+    liNode.appendChild(urlNode);
+    listNode.appendChild(liNode);
+
+    document.getElementById('serviceForm').submit(function (event) {
+        event.preventDefault();
+    });
+}
 
 
 // start: attribute query
@@ -486,24 +538,3 @@ function newaddRowHandlers() {
 // end: attribute query
 
 
-//Add Layer To List of Layers 
-
-function addLi() {
-
-    var title = document.getElementById('layerTitle').value,
-        url = document.getElementById('layerUrl').value;
-
-    var listNode = document.getElementById('layerUL');
-    var liNode = document.createElement("LI");
-
-    var titleNode = document.createTextNode(title),
-        urlNode = document.createTextNode(url);
-
-    liNode.appendChild(titleNode);
-    liNode.appendChild(urlNode);
-    listNode.appendChild(liNode);
-
-    document.getElementById('serviceForm').submit(function (event) {
-        event.preventDefault();
-    });
-}
